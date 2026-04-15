b6b95922e0addb8755b366ed8d3063dcae29e1c4
Mgt689 <Papa-Maguette_Diop@etu.u-bourgogne.fr>
2026-04-15 17:30:15 +0200

    Fix: Database configuration for production and add Python version

--- nettoyage_auxerre/settings.py
+++ nettoyage_auxerre/settings.py
@@ -88,12 +88,17 @@ WSGI_APPLICATION = 'nettoyage_auxerre.wsgi.application'
 
 import dj_database_url
 
-DATABASES = {
-    'default': dj_database_url.config(
-        default=f'sqlite:///{BASE_DIR / "db.sqlite3"}',
-        conn_max_age=600
-    )
-}
+if os.environ.get('DATABASE_URL'):
+    DATABASES = {
+        'default': dj_database_url.config(conn_max_age=600)
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
